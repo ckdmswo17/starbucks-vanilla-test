341e51c45be7e477630811779c4ed1600c863653
chakae <71180737+ckdmswo17@users.noreply.github.com>
2022-02-13 23:59:52 +0900

    visual setting
    4 day

--- js/main.js
+++ js/main.js
@@ -17,6 +17,30 @@ searchInputEl.addEventListener('blur',function() {
 
 const badgeEl = document.querySelector("header .badges");
 
-window.addEventListener('scroll',function(){ // window는 브라우저 창 객체 
-    
-})
+window.addEventListener('scroll',_.throttle(function(){
+    console.log(window.scrollY);
+    if(window.scrollY > 500) {
+        // 배지 숨기기
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+        
+    } else {
+        // 배지 보이기
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+},300));
+// _.throttle(함수, 시간(밀리세컨드))
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, indexx){
+    // gsap.to(요소, 지속시간, 옵션)
+    gsap.to(fadeEl, 1, {
+        delay: (indexx + 1) * .7, // 0.7, 1.4, 2.1, 2.7
+        opacity: 1
+    });
+});
