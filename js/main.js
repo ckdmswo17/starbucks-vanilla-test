eb5145c59f343df0e57b0753f8f4e9705b5806bb
chakae <71180737+ckdmswo17@users.noreply.github.com>
2022-02-18 00:04:27 +0900

    parallax setting
    7 day

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ _.throttle('scroll',function(){
     console.log(relativeTop);
 })},1);*/ // 범위밖 슬라이드를 안보이게 하려는 도전 -> 실패
 
-new Swiper('.promotion .swiper-container', {
+new Swiper(".promotion .swiper-container", {
     slidePerView: 3,
     spaceBetween: 10,
     centeredSlides: true,
@@ -95,3 +95,24 @@ promotionToggleBtn.addEventListener('click',function(){
         promotionEl.classList.remove('hide');
     }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+function floatingObject(selector, delay, size) {
+    gsap.to(selector // 선택자
+        ,random(1.5, 2.5) // 애니메이션 동작 시간
+        ,{ // 옵션
+        y: size,
+        repeat: -1,
+        yoyo: true,
+        ease: Power1.easeInOut,
+        delay: random(0, delay),      
+    });
+}
+floatingObject('.floating1',1,15);
+floatingObject('.floating2',5,15);
+floatingObject('.floating3',1.5,20);
